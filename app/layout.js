3b64c66a340e7f9501552abd3c3b979a216ed905
Emmanuel Inua <artofnuel@gmail.com>
2025-07-01 22:05:41 +0100

    Update to Seo again

--- app/layout.js
+++ app/layout.js
@@ -63,7 +63,7 @@ schedule—Aquatec has you covered. - Aquatec`,
     siteName: "Aquatec",
     images: [
       {
-        url: "/logo.png", // Place your image in the public folder
+        url: "../public/logo.png", // Place your image in the public folder
         width: 63.04,
         height: 93.22,
         alt: "Aquatec - Stay Hydrated, Anytime, Anywhere",
